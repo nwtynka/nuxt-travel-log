83c1c7c3a0d0e1fabe202d6ce33efc1860c12ef8
nwtynka <57356993+nwtynka@users.noreply.github.com>
2025-10-04 13:15:57 +0200

    feat/10 - add tailwind, daisyui and icons (#49)
    * feat/10 - add tailwind, daisyui and icons
    
    * feat/10 - add tailwind, daisyui and icons - fix

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-accent">
+      <Icon name="tabler:align-justified" />
+      Daisy UI button
+    </button>
+  </div>
+</template>
