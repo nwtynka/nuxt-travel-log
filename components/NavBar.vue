8e4da71d4cd290df599995d8eb1478bc8e4d194c
Kristyna <petrovak@magexo.cz>
2025-10-05 18:42:10 +0200

    feat/12: add dark mode toggle

---
+++ components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ThemeToggle from "./ThemeToggle.vue"
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <ThemeToggle />
+      <a class="btn btn-accent">
+        Sign In With Github
+        <Icon name="tabler:brand-github" size="24" />
+      </a>
+    </div>
+  </div>
+</template>
