4bcc8ff749b5d96491a2eee41e10f0f97e7e53ef
Kristyna <petrovak@magexo.cz>
2025-10-05 08:36:19 +0200

    feat/11: add basic layout

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-accent">
-      <Icon name="tabler:align-justified" />
-      Daisy UI button
-    </button>
-  </div>
+  <NuxtLayout name="default">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
